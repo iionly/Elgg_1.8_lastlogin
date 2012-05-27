14545a5f2a01afb9adc8e735eb050f6be66e15b6
Emmanuel <emanwebdev@gmail.com>
2012-05-27 17:10:37 +0200

    French language pack

---
+++ lastlogin/languages/fr.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Lastlogin plugin French language pack
+ *
+ * @package
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Fabrice Collette fabrice.collette@free.fr
+ * @copyright Fabrice Collette
+ *
+ * Elgg 1.8 version by iionly
+ * iionly@gmx.de
+ */
+
+$french = array(
+'lastlogin:online' => '- en ligne -',
+'lastlogin:lastlogin' => 'Dernière connexion',
+'lastlogin:today' => 'aujourd\'hui',
+'lastlogin:hour' => 'dans la dernière heure',
+'lastlogin:joindate' => 'Date d\'inscription',
+'lastlogin:guid' => 'GUID de ce membre',
+
+'lastlogin:settings:lastlogin_info' => 'Les paramètres suivants vous permettent de configurer les éléments à afficher sur les pages de profil: le temps du dernier login du membre, la date à laquelle il a rejoint le site et le GUID du membre <br> Pour le temps du dernier login et la date d\'inscription, vous pouvez choisir de ne pas l\'afficher ou de les afficher uniquement aux admins ou à tout le monde. Le GUID du membre peut être soit caché à tout le monde ou rendu visible seulement aux admins.',
+'lastlogin:settings:lastlogin_time' => 'Afficher date et heure du dernier login? ',
+'lastlogin:settings:registered_time' => 'Afficher date la d\'inscription? ',
+'lastlogin:settings:guid' => 'Afficher le GUID des membres (visible uniquement par les admins)? ',
+'lastlogin:settings:no' => 'Non',
+'lastlogin:settings:yes' => 'Oui',
+'lastlogin:settings:admin' => 'Pour les admins seulement',
+'lastlogin:settings:all' => 'Pour tout le monde',
+
+);
+
+add_translation("fr",$french);
