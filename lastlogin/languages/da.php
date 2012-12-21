dae7ed7316de7ab21979493e2a933987fe42d84b
Lars Hærvig <lars@laptus.dk>
2012-12-21 14:21:10 +0100

    Danish language file

---
+++ lastlogin/languages/da.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Lastlogin plugin
+ *
+ * @package
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Fabrice Collette fabrice.collette@free.fr
+ * @copyright Fabrice Collette
+ *
+ * Elgg 1.8 version by iionly
+ * iionly@gmx.de
+ */
+
+$danish = array(
+'lastlogin:online' => '- online -',
+'lastlogin:lastlogin' => 'Seneste aktivitet',
+'lastlogin:today' => 'idag',
+'lastlogin:hour' => 'indenfor den seneste time',
+'lastlogin:joindate' => 'Oprettet dato',
+'lastlogin:guid' => 'Medlem\'s GUID',
+'lastlogin:never' => 'Aldrig',
+
+'lastlogin:settings:lastlogin_info' => 'The following settings allow you to configure to display on profile pages the time of the member\'s last login, the date the member joined the site and the member\'s GUID.<br> For time of last login and join date you can choose to not display it or to display it either only for admins or for everyone. The member\'s GUID can either be hidden from everyone or made visible only for admins.',
+'lastlogin:settings:lastlogin_time' => 'Vis tidspunkt for seneste aktivitet? ',
+'lastlogin:settings:registered_time' => 'Vis oprettet dato? ',
+'lastlogin:settings:guid' => 'Vis medlemmets GUID (Kun synlig for admins)? ',
+'lastlogin:settings:no' => 'Nej',
+'lastlogin:settings:yes' => 'Ja',
+'lastlogin:settings:admin' => 'For admins only',
+'lastlogin:settings:all' => 'For alle',
+
+);
+
+add_translation("da",$danish);
